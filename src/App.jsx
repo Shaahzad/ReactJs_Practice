d1ff6454dc13b42ca045bba194e2b6a6078149d9
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-10-04 23:21:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import Counter from './Counter'
+
+
+
+// Most asked react js interview question
+ 
+// Q1  What is the difference between state and props in React? ✅
+// Q2  What is High order component in React?
+// Q3  What is the use of useEffect in React?
+// Q4  What is the use of useState in React?
+// Q5  What is the use of useRef in React?
+// Q6  What is the use of useCallback in React?
+// Q7  What is the use of useMemo in React?
+// Q8  What is the use of useReducer in React?
+// Q9 What is the use of useLayoutEffect in React?
+// Q10 what is propdilling in React?
+// Q11 what is context Api in React? 
+// Q12 what is redux in React?
+// Q13 what is router in React?
+// Q14 what is HOC in React?
+// Q15 what is functional component in React?
+// Q16 what is class component in React?
+// Q17 what is hooks in React?
+// Q18 what is lifecycle in React?
+// Q19 what is routing in React?
+// Q20 what is react-redux in React?
+const App = () => {
+  // const [count, setCount] = useState(0)
+  // return (
+  //   <div>
+  //     <button onClick={() => setCount(count + 1)}>Click</button>
+  //     <Counter count={count}/>
+  //   </div>
+  // )
+}
+
+export default App
---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// const Counter = ({count}) => {
+//   return (
+//     <div>
+//       <h1>Counter</h1>
+//       <p>{count}</p>
+//     </div>
+//   )
+// }
+
+// export default Counter
